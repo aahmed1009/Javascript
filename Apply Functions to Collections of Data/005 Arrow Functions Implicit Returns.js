3a87ecb6201fca8745543740e14b275189e9a1ec
Alaa Ahmed Gamal <61759061+aahmed1009@users.noreply.github.com>
2023-03-10 16:22:06 +0200

    Js

--- Apply Functions to Collections of Data/005 Arrow Functions Implicit Returns.js
+++ Apply Functions to Collections of Data/005 Arrow Functions Implicit Returns.js
@@ -6,3 +6,7 @@
 //expression expected
 const square = (x) => x * x;
 console.log(square(4));
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const EvenOrOdd = nums.map((x) => (x % 2 === 0 ? "Even" : "Odd"));
+console.log(EvenOrOdd);
